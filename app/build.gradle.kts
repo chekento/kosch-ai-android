2d2f216ac305f47ea9ea52d3e83415a03e9ea39e
chekento <152346316+chekento@users.noreply.github.com>
2026-08-25 09:31:51 +0200

    test: enable Compose instrumentation test foundation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 
     testOptions {
         unitTests.isIncludeAndroidResources = false
+        animationsDisabled = true
     }
 }
 
@@ -67,6 +68,15 @@ dependencies {
     implementation(libs.androidx.core.ktx)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     testImplementation(libs.junit)
+
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.androidx.test.core)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.rules)
+    androidTestImplementation(libs.androidx.test.espresso.core)
 }
