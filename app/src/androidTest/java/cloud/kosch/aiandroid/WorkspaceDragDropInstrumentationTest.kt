cca45473efa3d9da1de4348604a93d3ddc072d59
chekento <152346316+chekento@users.noreply.github.com>
2026-08-25 14:28:31 +0200

    M2.6 Stage D: fix drag instrumentation imports

--- app/src/androidTest/java/cloud/kosch/aiandroid/WorkspaceDragDropInstrumentationTest.kt
+++ app/src/androidTest/java/cloud/kosch/aiandroid/WorkspaceDragDropInstrumentationTest.kt
@@ -2,6 +2,7 @@ package cloud.kosch.aiandroid
 
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
