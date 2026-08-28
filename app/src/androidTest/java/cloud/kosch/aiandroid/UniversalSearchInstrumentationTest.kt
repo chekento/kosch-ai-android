fb624214aecf760022d09bf568f97d6500a95397
chekento <152346316+chekento@users.noreply.github.com>
2026-08-28 20:19:26 +0200

    Fix Universal Search instrumentation matcher import

--- app/src/androidTest/java/cloud/kosch/aiandroid/UniversalSearchInstrumentationTest.kt
+++ app/src/androidTest/java/cloud/kosch/aiandroid/UniversalSearchInstrumentationTest.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.lifecycle.ViewModelProvider
