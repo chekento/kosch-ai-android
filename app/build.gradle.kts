dd703e93cc091eb9773d2e2a01cdf5a2b954fa45
chekento <152346316+chekento@users.noreply.github.com>
2026-08-29 11:34:32 +0200

    build: bump hardened test candidate to alpha02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cloud.kosch.aiandroid"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.2.5-alpha01"
+        versionCode = 8
+        versionName = "0.2.5-alpha02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
